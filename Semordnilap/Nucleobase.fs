2fb9ba9603f08aa2e1e1241ccf441e0ee3d5915d
Manuel Messerer <Manuel.Messerer@gmail.com>
2021-02-08 09:34:38 +0100

    initial f# test

---
+++ Semordnilap/Nucleobase.fs
@@ -0,0 +1,38 @@
+﻿module Nucleobase
+
+//    module Purines
+
+//interface type INucleobase =
+//   //abstract member Add: int -> int -> int
+//   abstract (*member *)Sign: char
+//   // abstract read/write property
+//   //abstract member Area : float with get,set
+//end
+
+
+type INucleobase =
+    interface
+        abstract member Sign: char
+        abstract Opposite: INucleobase
+    end
+
+type Adenine() =
+    interface INucleobase with
+        member this.Sign = 'A'
+        member this.Opposite() = Thymine()
+        
+and Cytosine() =
+    interface INucleobase with
+        member this.Sign = 'C'
+        member this.Opposite() = Guanine()
+        
+and Guanine() =
+    interface INucleobase with
+        member this.Sign = 'G'
+        member this.Opposite() = Cytosine()
+
+and Thymine() =
+    interface INucleobase with
+        member this.Sign = 'T'
+        member this.Opposite() = Adenine()
+
